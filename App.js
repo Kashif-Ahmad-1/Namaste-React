1d040015d9e581567442068cdd737664729b08c8
Kashif Ahmad <19bcs2983@gmail.com>
2023-03-16 22:51:44 +0530

    React

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Heading 1 for parcel"
+);
+
+// const heading2 = React.createElement(
+//   "h2",
+//   {
+//     id: "title",
+//   },
+//   "Heading 2"
+// );
+
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//   },
+//   [heading, heading2]
+// );
+
+// console.log(heading);
+
+/*************************** */
+// JSX
+const heading2 = (
+  <h1 id="title" key="h2">
+    Namaste React 2
+  </h1>
+);
+console.log(heading2);
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+
+//async defer
+root.render(heading);
